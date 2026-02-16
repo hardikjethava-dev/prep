2570ae4ddfff43065fe3ad6608deb107bf30a0e9
hardikjethava-dev <hardik.jethava92@gmail.com>
2026-02-16 17:55:03 +0530

    optimization

--- DRF/inventory_saas/apps/organizations/models.py
+++ DRF/inventory_saas/apps/organizations/models.py
@@ -1,9 +1,8 @@
 from django.db import models
+from core.models import BaseModel
 
-class Organization(models.Model):
+class Organization(BaseModel):
     name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.name
